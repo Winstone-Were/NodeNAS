fbebd229958f1c7c6fde885e4d2b749b11c6222f
Winstone Were <stoniedev@gmail.com>
2024-02-03 08:39:35 +0300

    Added Multer to the project

--- Server.js
+++ Server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
 
 const App = express();
 App.use(cors());
+App.use(express.json());
+
+const upload = multer({dest:'uploads/'});
+
+App.post("/upload", upload.array("files"),uploadFiles);
+
+function uploadFiles(req,res){
+    
+}
 
 App.listen(8080,()=>{
     console.log('Server running on port 8080')
